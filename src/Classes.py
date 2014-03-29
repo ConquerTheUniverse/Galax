f77877a7790d1f3bf0c18e48d22151acb3fed3fa
Arnaud474 <arnaud.girardin444@gmail.com>
2014-03-29 17:41:01 -0400

    Analyse Linguistique + Squelette pour le code
    J'ai ecrit le mandat et j'ai fait un simple modele pour notre code ainsi
    que des "prototypes" pour les fonctions qui seront destinee a
    l'affichage du jeu.

---
+++ src/Classes.py
@@ -0,0 +1,19 @@
+#Contiendra toutes les informations relatives aux Humains (Joueur)
+class Humains:
+    pass
+
+#Contiendra toutes les informations relatives aux Gubrus
+class Grubus:
+    pass
+
+#Contiendras toutes les informations relatives aux Czins
+class Czin:
+    pass
+
+#Classe qui represente une flotte de vaisseau(Nb, Destination, etc.)
+class flotteDeVaisseaux:
+    pass
+
+#Classe qui represente une etoile(manufactures, vaisseaux, etc.)
+class etoile:
+    pass
--- src/Galax.py
+++ src/Galax.py
@@ -1,11 +1,34 @@
 from tkinter import *
+from Classes import *
 import random
 
 class Modele:
-    pass
+    def __init__(self, parent):
+        self.parent = parent
 
 class Vue:
-    pass
+    def __init__(self, parent):
+        self.parent = parent #Pour l'heritage provenant du Controleur
+
+    #Afficher le menu principal
+    def afficherMenu:
+        pass
+    
+    #Afficher les instructions pour le jeu
+    def instructions
+        pass
+
+    #Afficher le menu des options pour le jeu
+    def options
+        pass
+
+    #Affichage de la surface de jeu dans la fenetre
+    def afficherJeu:
+        pass
+
+    
+    
 
 class Controleur:
-    pass
+    def __init__(self):
+        pass
