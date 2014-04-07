49bd6a087c2b82de9c16bafe8c1a30713aea81e8
Arnaud474 <arnaud.girardin444@gmail.com>
2014-04-07 12:29:04 -0400

    Schema du GUI
    Image du schema du GUI

--- src/Classes.py
+++ src/Classes.py
@@ -1,19 +1,57 @@
 #Contiendra toutes les informations relatives aux Humains (Joueur)
 class Humains:
-    pass
+    def __init__(self):
+        pass
 
 #Contiendra toutes les informations relatives aux Gubrus
 class Grubus:
-    pass
+    def __init__(self):
+        pass
 
 #Contiendras toutes les informations relatives aux Czins
 class Czin:
-    pass
+    def __init__(self):
+        pass
 
 #Classe qui represente une flotte de vaisseau(Nb, Destination, etc.)
 class flotteDeVaisseaux:
-    pass
+    def __init__(self, quantiteVaisseaux, destination):
+        self.quantiteVaisseaux = quantiteVaisseaux
+        self.destination = destination
+        self.anneeArrivee = 0
+        
 
+    #Permet de calculer l'annee d'arrivee en fonction de la destination
+    def calculerArrivee(self, etoileDepart, anneeActuelle):
+        #Pour calculer la distance
+        differenceX = abs(etoileDepart.posX - self.destination.posX)
+        differenceY = abs(etoileDepart.posY - self.destination.posY)
+
+        #Si le X est plus grand
+        if(differenceX > differenceY):
+            distance = differenceX
+           
+        #Si le Y est plus grand ou si les deux sont egaux
+        else:
+            distance = differenceY
+
+        #Calculer l'anne d'arrivee
+        if(distance <= 2):
+            duree = distance / 2
+        else:
+            duree = 1 + ((distance- 2) / 3)
+
+        self.anneeArrivee = anneeActuelle + duree
+        
+
+        
+            
 #Classe qui represente une etoile(manufactures, vaisseaux, etc.)
 class etoile:
-    pass
+    def __init__(self, posX, posY, nbManufactures, appartenance)
+        self.posX = posX #Position en X de l'etoile sur la surface de jeu
+        self.posY = posY #Position en Y de l'etoile sur la surface de jeu
+        self.appartenance = appartenance #A qui cette etoile appartient (0 = Neutre, 1 = Humain, 2 = Gubrus et 3 = Czin)
+        self.nbManufactures = nbManufactures #Nb de manufactures sur l'etoile
+        self.nbVaisseaux = 0 #Nb de vaisseau sur l'etoile
+        
--- src/Galax.py
+++ src/Galax.py
@@ -5,6 +5,63 @@ import random
 class Modele:
     def __init__(self, parent):
         self.parent = parent
+        #Pour une surface de 1000x800 px ou les etoiles sont des cases de 10 x 10 px
+        self.surfaceX = 100
+        self.surfaceY = 80
+        self.nbEtoiles = 0
+        self.listeEtoiles = []
+        #Creation d'un objet pour chaque faction pour savoir differentes informations sur chacune des races
+        self.humain = Humain()
+        self.gubrus = Gubrus()
+        self.czin = Czin()
+
+    #Permet de creer toutes les etoiles au debut de la partie
+    def creerEtoiles(self):
+        #Creer un nombre d'etoile aleatoire entre 50 et 70
+        self.nbEtoiles = random.randint(50,70)
+
+        #Vider les ancinnes valeurs de la liste si il y en a
+        self.listeEtoiles[:] = []
+
+        #Creer le nombre d'etoile en fonction de la variable nbEtoiles(50 a 70)
+        for i in range(1, self.nbEtoiles):
+       
+
+            
+            #Permet de determiner si il y a une etoile a proximite, voir plus bas
+            aProximite = True
+
+            #Permet de creer une nouvelle position aleatoire tant que les etoiles sont trop proches
+            while(aProximite)
+
+                aProximite= False
+                
+                #Valeur aleatoire en X pour une etoile
+                x = random.randint(0, self.surfaceX-1)
+                #Valeur aleatoire en Y pour une etoile
+                y = random.randint(0, self.surfaceY-1)
+                
+                    
+                #Assurer que les etoiles ne soit pas directement les unes a cote des autres
+                for j in listeEtoiles:
+                    #Permet de regarder si la valeur absolue de la soustraction des deux positions est plus petite que 10 ex:abs(6-9)=3 et abs(9-6)=3
+                    if (abs(x-j.posX)<10 && abs(y-j.posY)<10):
+                        aProximite = True
+                        break; #Permet de sortir du for des qu'il y a une etoile trop proche
+
+
+            #Lorsqu'on reussit a trouver un x et un y qui est a la bonne distance on cree l'etoile a cette position 
+            nbM = random.randint(4,10) #Nombre aleatoire de Manufactures entre 4 et 10
+            self.listeEtoiles.append(x, y, nbM, 0)
+
+    #Fonction permettant de creer une etoile-mere pour chacune des factions(Humains, Grubus et Czin)         
+    def assignerLesEtoilesMeres(self):
+        pass
+                
+                
+            
+        
+        
 
 class Vue:
     def __init__(self, parent):
